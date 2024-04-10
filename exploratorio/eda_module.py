bcc728fdd97d53611d67d1ae969a6ac359fa2476
RouYi Wang <teresawang071101@gmail.com>
2024-04-10 22:49:02 +0200

    update eda

---
+++ exploratorio/eda_module.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+import numpy as np
+import matplotlib.gridspec as gridspec
+
+
+def my_function():
+  print("Hello from a function")
+    
+my_function()
